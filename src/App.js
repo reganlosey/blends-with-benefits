51baf6cf2612e726e3c96586346ed701783cbf14
Regan Losey <regan.losey@gmail.com>
2022-05-02 17:49:15 -0700

    Create header and css reset rules

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import './App.scss';
 import fetchData from './apiCalls';
 import Homepage from './Components/Homepage/Homepage';
 import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   const [brewData, setBrewData] = useState([]);
 
   const getBrewsData = async () => {
     const resp = await fetchData("https://brewedtoserve.herokuapp.com/brews")
-    console.log(resp)
-
+    setBrewData(resp)
   }
 
   useEffect(() => {
     getBrewsData()
   }, [])
   return (
-    <h1>Grind So Fine</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </div>
   )
 
 }
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,9 +1,24 @@
 import './Homepage.scss'
+import {NavLink} from 'react-router-dom';
 
 
 const Homepage = () => {
   return (
-    <h1>Homepage!</h1>
+    <div className="homepage">
+      <nav className="nav-bar">
+      <NavLink to="/">bwb</NavLink>
+      </nav>
+      <section className="welcome-msg">
+        <h1>Hey there, brew-tiful</h1>
+        <h2>Welcome to Blends With Benefits</h2>
+        <p>
+          We know that the daily grind can be exhausting and finding the right cup of coffee or tea be a tall order.
+        </p>
+        <p>
+          But don't worry, we're here to help you to find your one and only.
+        </p>
+      </section>
+    </div>
   )
 
 }
