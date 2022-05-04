0d2616e70b6e661390b0a4864d31e2791d4160ed
Regan Losey <regan.losey@gmail.com>
2022-05-04 16:04:49 -0700

    Adjust BEM classNames for cards

--- src/Components/Brew/Brew.js
+++ src/Components/Brew/Brew.js
@@ -2,7 +2,7 @@ import './Brew.scss'
 
 const Brew = ({ id, productName, type, price, hasCaffeine }) => {
   return (
-    <article className={type === 'Tea' ? "brew-card__tea" : "brew-card__coffee"}>
+    <article className={type === 'Tea' ? "brew-card brew-card--tea" : "brew-card brew-card--coffee"}>
       <p className="brew-info__name">{productName}</p>
       <p className="brew-info__type">{type}</p>
       <p className="brew-info__price">Price: ${price}</p>
