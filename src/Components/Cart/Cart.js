4b56b0604a9ab33b82541a4af200abcf580879b6
Regan Losey <regan.losey@gmail.com>
2022-05-05 12:11:54 -0700

    Create cart card layout

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,13 +1,26 @@
 import './Cart.scss'
 
-const Cart = ({cartItems}) => {
+const Cart = ({ cartItems }) => {
   console.log(cartItems)
-  return(
-    <div className='cart'>
-      <h1> Your Cart </h1>
+  const allItems = cartItems.map((item) => {
+    return (
+      <div className="single-item"
+        key={item.id}
+      >
+      </div>
+    )
+  })
+  return (
+    <div className="cart">
+        <div className="cart-card">
+          <div className="cart-card__cart-header">
+          <h3 className="cart-info--heading">Your Cart</h3>
+          <h4 className="cart-info--action">Remove All</h4>
+          </div>
+          {allItems}
+        </div>
     </div>
   )
-
 
 }
 
