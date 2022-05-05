052ab69e60defe181eb94c723a88048c3e89cc93
Regan Losey <regan.losey@gmail.com>
2022-05-05 12:51:14 -0700

    Position cart details

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -12,13 +12,32 @@ const Cart = ({ cartItems }) => {
   })
   return (
     <div className="cart">
-        <div className="cart-card">
-          <div className="cart-card__cart-header">
+      <div className="cart-card">
+        <div className="cart-card__cart-header">
           <h3 className="cart-info--heading">Your Cart</h3>
           <h4 className="cart-info--action">Remove All</h4>
-          </div>
-          {allItems}
         </div>
+        <div className="cart-content">
+          <div className="cart-content--item-image"></div>
+          <img />
+          <div className="item-details">
+            ITEM DETAILS
+            <p className="item-details--item-name">Coffee</p>
+            <p className="item-details--item-pounds">2lbs</p>
+          </div>
+          <div className="cart-counter">
+            CART COUNTER
+            <button className="cart-counter--increase-btn">+</button>
+            <button className="cart-counter--decrease-btn">-</button>
+          </div>
+          <div className="cart-prices">
+            CART PRICES
+            <p className="cart-prices--item-price">$10/lb</p>
+            <button className="cart-prices--remove-item">Remove</button>
+          </div>
+        </div>
+        {allItems}
+      </div>
     </div>
   )
 
