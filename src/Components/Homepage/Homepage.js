252e6d19dba913d710f489423341338f2acacd9f
Regan Losey <regan.losey@gmail.com>
2022-05-04 17:20:44 -0700

    Layout JSX for Homepage.js

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,21 +1,25 @@
 import './Homepage.scss'
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <section className="welcome-msg">
-        <h1>Hey there, brew-tiful</h1>
-        <h2>Welcome to Blends With Benefits</h2>
-        <p>
+      <div className="intro">
+        <h1 className="intro--heading">Hey there, hot stuff</h1>
+        <h2 className="intro-subheading">Welcome to</h2>
+        <p className="intro--site-name">Blends With Benefits</p>
+        {/* <p className="intro--details">
           We know that the daily grind can be exhausting and finding the right cup of coffee or tea be a tall order.
         </p>
-        <p>
+        <p className="intro--details">
           But don't worry, we're here to help you to find your one and only.
-        </p>
-      </section>
+        </p> */}
+      </div>
       <div className="get-started">
-        <Link to="/shop">Browse All Brews</Link>
+        {/* <p>Let's get started</p> */}
+        <button className="get-started--start-btn">
+          <Link to="/shop">Browse All Brews</Link>
+        </button>
       </div>
     </div>
   )
