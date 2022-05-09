9b8172ba56d84254147b63aaf9fb11c4400cd32a
Regan Losey <regan.losey@gmail.com>
2022-05-09 16:55:19 -0700

    Render order cards to page

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,10 +1,24 @@
 import './Orders.scss';
-import { useSelector } from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 
 
 const Orders = () => {
+  const allOrders = useSelector((state) => state.orders.allOrders)
+  const orderCards = allOrders.map((order) => {
+    console.log(order)
+      return <article className="order-card"
+      key={Math.floor(Math.random() * Date.now())}
+      >
+        <h1>{order.map((item) => item.productName)}</h1>
+      </article>
+      
+  })
+
+
   return (
-    <h1>Orders!</h1>
+    <div>
+      {orderCards}
+    </div>
   )
 }
 
