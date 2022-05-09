aa1d1ddb9362ec530fa3af6ed89035972dc217ea
Regan Losey <regan.losey@gmail.com>
2022-05-08 19:11:08 -0700

    Refactor addItem reducer

--- src/state/cartSlice.js
+++ src/state/cartSlice.js
@@ -8,18 +8,20 @@ export const cartSlice = createSlice({
 
   reducers: {
     addItemToCart: (state, action) => {
-      const foundItem = state.findIndex((item) => item.id === action.payload.id)
-      if (!state[foundItem]) {
+      const foundItem = state.find((item) => item.id === action.payload.id)
+      if (!foundItem) {
         action.payload.quantity = 1
-        return state.concat([action.payload])
-      } else if (state[foundItem]) {
-        state[foundItem].quantity += 1
+        return state = [...state, action.payload]
+      } else if (foundItem) {
+        foundItem.quantity += 1;
       }
     },
     removeItemFromCart: (state, action) => {
-      const foundItem = state.findIndex((item) => item.id === action.payload.id)
-      if (state[foundItem]) {
-        state[foundItem].quantity -=1
+      const foundItem = state.find((item) => item.id === action.payload.id)
+      if (foundItem) {
+        foundItem.quantity -= 1
+      } else {
+        throw new Error ("Item not found")
       }
     }
   },
