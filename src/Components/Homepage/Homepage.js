44e303ba850fdfd0642213d19bba8b3f61c0ea18
Regan Losey <regan.losey@gmail.com>
2022-05-12 16:10:36 -0700

    Link buttons to respective routes

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,18 +1,16 @@
 import './Homepage.scss'
 import { Link } from 'react-router-dom';
-import siteSvg from '../../assets/site_name.svg'
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <div className="intro">
         <p className="intro--heading">Hey There Hot Stuff, Welcome To</p>
-        {/* <p className="intro--subheading">Welcome to</p> */}
         <h1 className="intro--site-name">BLENDS WITH BENEFITS</h1>
       </div>
       <div className="browse-wrapper">
-        <button className="browse-wrapper--coffee-btn">Browse Coffee</button>
-        <button className="browse-wrapper--tea-btn">Browse Tea</button>
+        <button className="browse-wrapper--coffee-btn"><Link to="/shop/coffee">Browse Coffee</Link></button>
+        <button className="browse-wrapper--tea-btn"><Link to="/shop/tea">Browse Tea</Link></button>
       </div>
 
     </div>
