00119f8be8ea24bf4cb356d2a605283457626693
Regan Losey <regan.losey@gmail.com>
2022-05-12 15:44:28 -0700

    Adjust colors of background, text, and position browse text

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,17 +1,20 @@
 import './Homepage.scss'
 import { Link } from 'react-router-dom';
+import siteSvg from '../../assets/site_name.svg'
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <div className="intro">
-        <p className="intro--site-name">Blends With Benefits</p>
+        <p className="intro--heading">Hey There Hot Stuff, Welcome To</p>
+        {/* <p className="intro--subheading">Welcome to</p> */}
+        <h1 className="intro--site-name">BLENDS WITH BENEFITS</h1>
       </div>
-      <div className="get-started">
-        <button className="get-started--start-btn">
-          <Link to="/shop">Browse All Brews</Link>
-        </button>
+      <div className="browse-wrapper">
+        <p className="browse-wrapper--coffee-txt">Browse Coffee</p>
+        <p className="browse-wrapper--tea-txt">Browse Tea</p>
       </div>
+
     </div>
   )
 
