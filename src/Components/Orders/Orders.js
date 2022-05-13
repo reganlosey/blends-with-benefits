b29184340a580de45ced7f39201cb8191d6fcd22
Regan Losey <regan.losey@gmail.com>
2022-05-13 14:31:39 -0700

    Change layout of order card containers, sheesh

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -13,33 +13,28 @@ const Orders = () => {
   }
 
   const orderCards = allOrders.map((order, index) => {
-    return <article className="order-content"
+    return <div className="order-items"
       key={index + 1}
-    >ORDER CONTENT
-      <div className="order-items"> ORDER ITEMS
-        <h1 className="order-items--summary-text">Order Summary</h1>
-        <p className="order-items--items-ordered">Items Ordered</p>
-        <p className="order-items--items-list">{order.map((item) => `${item.productName} : ${item.quantity} lbs`)} </p>
-        <p className="order-items--order-date"> Placed On: {new Date(order[0].orderDate).toDateString()}</p>
-        <p className="order-items--order-total">Order Total: {formatPrice(order[0].subTotal + (order[0].subTotal * .08))}</p>
-      </div>
-    </article>
+    >
+      <h1 className="order-items--summary-text">Order #{index + 1}</h1>
+      <p className="order-items--items-ordered">Items Ordered</p>
+      <p className="order-items--items-list">{order.map((item) => `${item.productName} : ${item.quantity} lbs`)} </p>
+      <p className="order-items--order-date"> Placed On: {new Date(order[0].orderDate).toDateString()}</p>
+      <p className="order-items--order-total">Order Total: {formatPrice(order[0].subTotal + (order[0].subTotal * .08))}</p>
+    </div>
   })
 
 
   return (
     <div className="orders">
-      ORDERS
-      <div className="order-card"> ORDER CARD
-        <h3 className="order-card__header"> HEADER
-        </h3>
-        <div className="order-content-container">
-          ORDER CONTENT Container
-          {allOrders.length ? orderCards : <p className="empty-order-txt">Awful quiet in here...</p>}
+      <div className="order-card">
+        <h3 className="order-card--header"> Your Orders</h3>
+        <div className="order-card--inner">
+          {allOrders.length ? orderCards : <p className="empty-orders-txt">Awful quiet in here...</p>}
         </div>
-
       </div>
     </div>
+
   )
 }
 
