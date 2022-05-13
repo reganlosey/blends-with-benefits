6491db37a3f939f457168df2642e14eb46306d18
Regan Losey <regan.losey@gmail.com>
2022-05-13 13:33:33 -0700

    Adjust conditional for setting shippingCost

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -35,9 +35,9 @@ const Cart = () => {
     }, 0)
 
     setSubTotal(totals)
-    if (totals > 0 && totals < 20) {
+    if (totals < 20 && totals > 0 ) {
       setShippingCost(5)
-    } else {
+    } else if (totals > 20){
       setShippingCost(0)
     }
     return subTotal
