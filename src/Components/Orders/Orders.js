e7200745c9af72b9e9f796f02610e8528c0a69f4
Regan Losey <regan.losey@gmail.com>
2022-05-13 14:09:22 -0700

    Add height and width to order card

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,12 +1,9 @@
 import './Orders.scss';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
 
 
 const Orders = () => {
   const allOrders = useSelector((state) => state.orders.allOrders)
-  const [orderDate, setOrderDate] = useState('');
-  const [subTotal, setSubTotal] = useState(0);
 
   const formatPrice = (price) => {
     return price.toLocaleString('en-US', {
@@ -14,26 +11,34 @@ const Orders = () => {
       currency: "USD"
     })
   }
-  
+
   const orderCards = allOrders.map((order, index) => {
-    return <article className="order-card"
+    return <article className="order-content"
       key={index + 1}
-    >
-      <div className="order-items">
+    >ORDER CONTENT
+      <div className="order-items"> ORDER ITEMS
         <h1 className="order-items--summary-text">Order Summary</h1>
         <p className="order-items--items-ordered">Items Ordered</p>
         <p className="order-items--items-list">{order.map((item) => `${item.productName} : ${item.quantity} lbs`)} </p>
         <p className="order-items--order-date"> Placed On: {new Date(order[0].orderDate).toDateString()}</p>
         <p className="order-items--order-total">Order Total: {formatPrice(order[0].subTotal + (order[0].subTotal * .08))}</p>
-        <h1></h1>
       </div>
     </article>
   })
 
 
   return (
-    <div className="order-card-container">
-      {allOrders.length ? orderCards : <p className="empty-order-txt">Awful quiet in here...</p>}
+    <div className="orders">
+      ORDERS
+      <div className="order-card"> ORDER CARD
+        <h3 className="order-card__header"> HEADER
+        </h3>
+        <div className="order-content-container">
+          ORDER CONTENT Container
+          {allOrders.length ? orderCards : <p className="empty-order-txt">Awful quiet in here...</p>}
+        </div>
+
+      </div>
     </div>
   )
 }
