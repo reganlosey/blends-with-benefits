750a9347b71e9faf7b3d6088c3da4dab56543e3d
Regan Losey <regan.losey@gmail.com>
2022-05-14 11:17:34 -0700

    Test that cart items have correct quantity and move command to before hook

--- cypress/integration/Cart_spec.js
+++ cypress/integration/Cart_spec.js
@@ -1,13 +1,28 @@
 describe('Cart', () => {
+  before(() => {
+    cy.addToCart()
+  })
   beforeEach(() => {
     cy.intercept('https://brewedtoserve.herokuapp.com/brews', {
         fixture: 'brews'
       })
       .visit('http://localhost:3000/cart/')
-      cy.addToCart()
   })
 
-  it('should have a sample test', () => {
-    cy.url('eq','http://localhost:3000/cart/')
+  it('cart should contain all items that were clicked', () => {
+    cy.get('.cart')
+    .get('.cart-content-container')
+      .children('.cart-item')
+      .then(cartItems => expect(cartItems.length).to.eq(12))
+      .reload()
+  })
+
+  it('should have the correct number of pounds per item', () => {
+    cy.get('.cart-content-container')
+    .children('.cart-item')
+    .each(() => {
+      cy.get('.cart-counter > .cart-counter--num-items')
+      .contains('1 lbs')
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
 Cypress.Commands.add('addToCart',(addButton) => {
   cy.intercept('https://brewedtoserve.herokuapp.com/brews', {
         fixture: 'brews'
@@ -24,18 +13,5 @@ Cypress.Commands.add('addToCart',(addButton) => {
           return
         }
       })
+      .visit('http://localhost:3000/cart/')
 })
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
