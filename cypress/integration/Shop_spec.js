a178ddc89518e83a25bd8baef02ddffb938a019b
Regan Losey <regan.losey@gmail.com>
2022-05-14 10:23:02 -0700

    Test that only tea cards are rendered on visit to shop/tea

--- cypress/integration/Shop_spec.js
+++ cypress/integration/Shop_spec.js
@@ -1,7 +1,9 @@
 describe('Shop', () => {
   beforeEach(() => {
-    cy.intercept('https://brewedtoserve.herokuapp.com/brews', { fixture: 'brews' })
-    .visit('http://localhost:3000/shop/')
+    cy.intercept('https://brewedtoserve.herokuapp.com/brews', {
+        fixture: 'brews'
+      })
+      .visit('http://localhost:3000/shop/')
   })
 
   it('should have a sample test', () => {
@@ -9,17 +11,30 @@ describe('Shop', () => {
   })
   it('should render all coffee and tea cards', () => {
     cy.get('.brew-cards')
-    .children('.brew-card')
-    .then(cards => expect(cards.length).to.eq(12))
+      .children('.brew-card')
+      .then(cards => expect(cards.length).to.eq(12))
   })
-  
+
   it('all cards should contain a button to add the item to cart', () => {
     cy.get('.brew-cards')
-    .children('.brew-card')
-    .each(() => {
-      cy.get('.brew-info > .brew-info__body > .brew-card--cart-btn')
-      .should('contain', 'Add To Cart')
-    })
+      .children('.brew-card')
+      .each(() => {
+        cy.get('.brew-info > .brew-info__body > .brew-card--cart-btn')
+          .should('contain', 'Add To Cart')
+      })
+  })
+
+  it('should only render tea cards on visit to shop/tea', () => {
+    cy.visit('http://localhost:3000/shop/tea')
+      .get('.brew-cards')
+      .children('.brew-card')
+      .then(teaCards => expect(teaCards.length).to.eq(7))
+      .each(() => {
+        cy.get('.brew-info > .brew-info__body')
+          .should('contain', 'Tea')
+          .and('contain', '$10/lb')
+      })
+
   })
 
 })
