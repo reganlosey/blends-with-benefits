246515d1225f6fe135b90867166f62361ab524d7
Regan Losey <regan.losey@gmail.com>
2022-05-13 21:36:47 -0700

    Write tests to assert buttons link to correct urls

--- cypress/integration/Homepage_spec.js
+++ cypress/integration/Homepage_spec.js
@@ -5,4 +5,24 @@ describe("Homepage", () => {
   it('should have a sample test', () => {
     expect(true).to.equal(true)
   })
+
+  it('should display the greeting and site name', () => {
+    cy.get('.intro')
+    .get('.intro--heading').contains('Hey There Hot Stuff, Welcome To')
+    .get('.intro--site-name').contains('BLENDS WITH BENEFITS')
+  })
+  it('should have buttons that link to /shop:coffee and /shop:tea', ( ) => {
+    cy.get('.browse-wrapper')
+    .get('.browse-wrapper--coffee-btn')
+    .contains('Browse Coffee')
+    .click()
+    .url("eq", "http://localhost:3000/shop/coffee")
+    cy.go('back')
+    cy.get('.browse-wrapper')
+    .get('.browse-wrapper--tea-btn')
+    .contains('Browse Tea')
+    .click()
+    .url("eq", "http://localhost:3000/shop/tea")
+    
+  })
 })
