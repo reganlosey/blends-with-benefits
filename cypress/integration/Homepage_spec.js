90fc420315fedba04728456f7a689e0a03adead9
Regan Losey <regan.losey@gmail.com>
2022-05-13 22:11:27 -0700

    Test Homepage display

--- cypress/integration/Homepage_spec.js
+++ cypress/integration/Homepage_spec.js
@@ -6,23 +6,28 @@ describe("Homepage", () => {
     expect(true).to.equal(true)
   })
 
+  it('should display the background image', () => {
+    cy.get('html')
+      .should('have.css', 'background-image')
+  })
+
   it('should display the greeting and site name', () => {
     cy.get('.intro')
-    .get('.intro--heading').contains('Hey There Hot Stuff, Welcome To')
-    .get('.intro--site-name').contains('BLENDS WITH BENEFITS')
+      .get('.intro--heading').contains('Hey There Hot Stuff, Welcome To')
+      .get('.intro--site-name').contains('BLENDS WITH BENEFITS')
   })
-  it('should have buttons that link to /shop:coffee and /shop:tea', ( ) => {
+  
+  it('should have buttons that link to /shop:coffee and /shop:tea', () => {
     cy.get('.browse-wrapper')
-    .get('.browse-wrapper--coffee-btn')
-    .contains('Browse Coffee')
-    .click()
-    .url("eq", "http://localhost:3000/shop/coffee")
+      .get('.browse-wrapper--coffee-btn')
+      .contains('Browse Coffee')
+      .click()
+      .url("eq", "http://localhost:3000/shop/coffee")
     cy.go('back')
     cy.get('.browse-wrapper')
-    .get('.browse-wrapper--tea-btn')
-    .contains('Browse Tea')
-    .click()
-    .url("eq", "http://localhost:3000/shop/tea")
-    
+      .get('.browse-wrapper--tea-btn')
+      .contains('Browse Tea')
+      .click()
+      .url("eq", "http://localhost:3000/shop/tea")
   })
 })
