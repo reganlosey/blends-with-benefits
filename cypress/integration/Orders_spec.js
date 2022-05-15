6839c7bc5866446a3d7e96ddc06a454ceb71aa1e
Regan Losey <regan.losey@gmail.com>
2022-05-14 17:47:25 -0700

    Test that order card is present with items from placed order

--- cypress/integration/Orders_spec.js
+++ cypress/integration/Orders_spec.js
@@ -8,15 +8,20 @@ describe('Orders', () => {
 
   it('should render an order card', () => {
     cy.get('.order-card')
-    .get('.order-card--order-num')
-    .should('exist')
-    .get('.order-card--total')
-    .should('contain', "Order Total: $70.20")
-    .get('.order-card--order-date')
-    .should('contain', '2022')
-    .get('.items-ordered-txt')
-    .should('exist')
+      .get('.order-card--order-num')
+      .should('exist')
+      .get('.order-card--total')
+      .should('contain', "Order Total: $70.20")
+      .get('.order-card--order-date')
+      .should('contain', '2022')
+      .get('.items-ordered-txt')
+      .should('exist')
   })
 
-  it('')
+  it('should contain the items from the placed order', () => {
+    cy.get('.order-summary')
+    .children()
+    .then(orders => expect(orders.length).to.eq(6))
+
+  })
 })
