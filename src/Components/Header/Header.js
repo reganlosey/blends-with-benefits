f0d85e8aa143ea28e7ecc61972fb97ba18d68025
Regan Losey <regan.losey@gmail.com>
2022-05-15 12:46:42 -0700

    Create images for header links and align on baseline of header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,19 +1,25 @@
 import './Header.scss'
-import siteLogo from '../../assets/site-logo-txt.svg';
+import siteLogo from '../../assets/site-logo-vctr.svg';
+import ordersText from '../../assets/order-txt-vctr.svg';
+import cartText from '../../assets/cart-txt-vctr.svg'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
-      {/* <div className="header--left">
-      </div> */}
-      <div className="header--right">
-        <Link to="/">
-          <img className="site-logo" src={siteLogo} alt="the letters BWB" />
-        </Link>
-        <Link to="/orders">Orders</Link>
-        <Link to="/cart">Cart</Link>
+      <div className="header-left">
+        <img className="site-logo" src={siteLogo} alt="the letters BWB" />
       </div>
+      <div className="header-right">
+        <Link to="/orders">
+          <img className="orders-img" src={ordersText} alt="img of the word orders" />
+        </Link>
+        <Link to="/cart">
+          <img className="cart-img" src={cartText} alt="img of the word cart" />
+        </Link>
+      </div>
+
+
     </header>
   )
 
