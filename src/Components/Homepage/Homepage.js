3a100682130920b6df8ad8ebe79abdb961c115a0
Regan Losey <regan.losey@gmail.com>
2022-05-15 19:09:02 -0700

    Change homepage heading to lowercase

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -5,8 +5,7 @@ const Homepage = () => {
   return (
     <div className="homepage">
       <div className="intro">
-        <p className="intro--heading">Hey there hot stuff, </p>
-        <p className="intro--subheading">welcome to</p>
+        <p className="intro--heading">Hey there hot stuff, welcome to</p>
         <h1 className="intro--site-name">BLENDS WITH BENEFITS</h1>
       </div>
       <div className="browse-wrapper">
