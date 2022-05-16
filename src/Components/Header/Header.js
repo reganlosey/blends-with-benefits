de888844a2b94cc45875decfd21f55ef1016b774
Regan Losey <regan.losey@gmail.com>
2022-05-16 16:18:32 -0700

    Change header to have bg color and black border, add active effects

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,27 @@
 import './Header.scss'
 import siteLogo from '../../assets/site-logo-vctr.svg';
-import ordersText from '../../assets/orders-txt-vctr.png';
-import cartText from '../../assets/cart-txt-vctr.png'
-import { NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
 
   return (
     <header className="header">
       <div className="header-left">
-        <NavLink to="/" className="header-link" >
+        <NavLink to="/" className="home-link" >
           <img className="site-logo" src={siteLogo} alt="the letters BWB" />
         </NavLink>
       </div>
+      <div className="header-center">
+        <NavLink to="shop/coffee" className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--coffee' : 'nav-link nav-link--coffee'}>Shop Coffee</NavLink>
+        <NavLink to="shop/tea" className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--tea' : 'nav-link nav-link--tea'}>Shop Tea</NavLink>
+        <NavLink to="/shop/" className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--shop' : 'nav-link nav-link--shop'}>Shop All</NavLink>
+      </div>
       <div className="header-right">
-        <NavLink to="/orders" className={(navData) => navData.isActive ? 'header-active' : 'orders-link'}>
-          {/* <p className="orders-link-txt">ORDERS</p> */}
-          ORDERS
-          {/* <img className="orders-img" src={ordersText} alt="img of the word orders" /> */}
+        <NavLink to="/orders" className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--orders' : 'nav-link nav-link--orders'}>
+          Orders
         </NavLink>
-        <NavLink to="/cart" className={(navData) => navData.isActive ? 'header-active' : 'cart-link'} >
-          {/* <img className="cart-img" src={cartText} alt="img of the word cart" /> */}
-          <p className="cart-link-txt">CART</p>
+        <NavLink to="/cart" className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--cart' : 'nav-link nav-link--cart'} >
+          Cart
         </NavLink>
       </div>
 
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -6,7 +6,7 @@ const Homepage = () => {
     <div className="homepage">
       <div className="intro">
         <p className="intro--heading">Hey there hot stuff, welcome to</p>
-        <h1 className="intro--site-name">BLENDS WITH BENEFITS</h1>
+        <h1 className="intro--site-name">Blends With Benefits</h1>
       </div>
       <div className="browse-wrapper">
         <button className="browse-wrapper--coffee-btn"><Link to="/shop/coffee">Browse Coffee</Link></button>
