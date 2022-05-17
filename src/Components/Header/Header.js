0e7669dadce93327d72bc76add0a81acaed48c56
Regan Losey <regan.losey@gmail.com>
2022-05-17 16:03:09 -0700

    Allow me to achieve contentment with the header and homepage

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,21 +12,21 @@ const Header = () => {
         </NavLink>
       </div>
       <div className="header-center">
+        <NavLink to="/shop/"
+          className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--shop' : 'nav-link nav-link--shop'}>All Blends</NavLink>
         <NavLink to="shop/coffee"
           className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--coffee' : 'nav-link nav-link--coffee'}>Coffee</NavLink>
         <NavLink to="shop/tea"
           className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--tea' : 'nav-link nav-link--tea'}>Tea</NavLink>
-        <NavLink to="/shop/"
-          className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--shop' : 'nav-link nav-link--shop'}>All Blends</NavLink>
       </div>
       <div className="header-right">
-        <NavLink to="/orders"
-          className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--orders' : 'nav-link nav-link--orders'}>
-          Orders
-        </NavLink>
         <NavLink to="/cart"
           className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--cart' : 'nav-link nav-link--cart'} >
           Cart
+        </NavLink>
+        <NavLink to="/orders"
+          className={(navData) => navData.isActive ? 'nav-link-a nav-link-a--orders' : 'nav-link nav-link--orders'}>
+          Orders
         </NavLink>
       </div>
 
