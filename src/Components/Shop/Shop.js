6f228507c159626372e772381a0a9601f3c4855a
Regan Losey <regan.losey@gmail.com>
2022-05-18 09:34:24 -0700

    Remove comment and unused assets

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -24,7 +24,7 @@ const Shop = ({ addToCart }) => {
     } else if (!params.query && !arr.includes(brewCard)) {
       arr.push(brewCard)
     } else {
-      // navigate('/*')
+
     }
     return arr
   }, [])
